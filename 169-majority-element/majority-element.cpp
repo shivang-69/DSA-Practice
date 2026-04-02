9f3a579cd575601719744751ca9455f83a984c8f
shivang-69 <shivang63501@gmail.com>
2026-04-03 00:51:01 +0530

    Time: 2 ms (32.22%) | Memory: 28.3 MB (7.82%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        unordered_map<int,int>mp;
+
         int n=nums.size();
 
-        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){ 
+            mp[nums[i]]++;
+            if(mp[nums[i]]>n/2) return nums[i];
 
-        return nums[n/2];
-        
-
+            }
+        return 0;
         
     }
 };
