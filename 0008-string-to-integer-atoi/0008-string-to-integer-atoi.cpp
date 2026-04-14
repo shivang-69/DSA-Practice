c2e54914a618e0594e8a260fccd78bbfd5077f07
shivang-69 <shivang63501@gmail.com>
2026-04-15 03:09:01 +0530

    Time: 0 ms (100%), Space: 9.1 MB (81.18%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -8,12 +8,12 @@ public:
         long result = 0;
         int sign = 1;
 
-        // 1. Skip leading whitespaces
+      
         while (i < n && s[i] == ' ') {
             i++;
         }
 
-        // 2. Handle optional sign
+      
         if (i < n && (s[i] == '+' || s[i] == '-')) {
             if (s[i] == '-') {
                 sign = -1;
@@ -21,11 +21,10 @@ public:
             i++;
         }
 
-        // 3. Process digits
+        
         while (i < n && isdigit(s[i])) {
             int digit = s[i] - '0';
 
-            // 4. Handle overflow
             if (result > (INT_MAX - digit) / 10) {
                 return (sign == 1) ? INT_MAX : INT_MIN;
             }
