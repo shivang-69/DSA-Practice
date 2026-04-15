158fe1a74a664870c0640e87c21deeae89b4c38f
shivang-69 <shivang63501@gmail.com>
2026-04-15 15:27:18 +0530

    Time: 3 ms (65.92%), Space: 12.2 MB (43.7%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+
+        string ans="";
+
+        vector<int>val={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+             vector<string> sym = {"M", "CM", "D", "CD", "C", "XC",
+                              "L", "XL", "X", "IX", "V", "IV", "I"};
+
+
+        for(int i=0;i<13;i++){
+
+            if(num==0) break;
+
+            int times=num/val[i];
+
+            while(times>0){
+                ans+=sym[i];
+                times--;
+            }
+
+            num%=val[i];
+
+
+        }
+
+        return ans;
+
+        
+    }
+};
