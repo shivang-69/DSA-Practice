13586e1e4521807729c09a341e838d66abbd2219
shivang-69 <shivang63501@gmail.com>
2026-04-16 18:29:41 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (34.55%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -2,14 +2,50 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
 
+        int pivot=0;
+
         int n=nums.size();
 
-        for(int i=0;i<n;i++){
-             if(nums[i]==target) return i;
+        for(int i=0;i<n-1;i++){
+            if(i+1<n && nums[i]>nums[i+1])  pivot=i+1;
+        }
+
+        int l=0;
+        int r=pivot-1;
+
+           while(l<=r){
+            int mid=l+(r-l)/2;
+
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) l=mid+1;
+
+            else  r=mid-1;
+            
+
             }
+
+
+            l=pivot;
+            r=n-1;
+
+
+        while(l<=r){
+            int mid=l+(r-l)/2;
+
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) l=mid+1;
+
+            else  r=mid-1;
+            
+
+            }
+
+
+            return -1;
+
         
 
-        return -1;
+        
         
     }
 };
