cb62fdf03522d657b91ead0944b75c06836f698c
shivang-69 <shivang63501@gmail.com>
2026-08-27 01:17:06 +0530

    Time: 0 ms (100%), Space: 9.2 MB (98.86%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,59 +1,54 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-
-                int m = matrix.size();
-        int n = matrix[0].size();
-        vector<int> result;
-        int top   = 0;
-        int down  = m-1;
-        int left  = 0;
-        int right = n-1;
         
-        int id = 0;
-        //id
-        //0   -> left  to right
-        //1   -> top   to down
-        //2   -> right to left
-        //3   -> down  to top
+        vector<int>ans;
+           int m=matrix.size();
+           int n=matrix[0].size();
 
-        while(top <= down && left <= right) {
-            //left to right
-            if(id == 0) {
-                for(int i = left; i<=right; i++) {
-                    result.push_back(matrix[top][i]);
-                }
-                top++;
-            }
-            
-            //top to down
-            if(id == 1) {
-                for(int i = top; i<=down; i++) {
-                    result.push_back(matrix[i][right]);
-                }
-                right--;
-            }
-            
-            //right to left
-            if(id == 2) {
-                for(int i = right; i>=left; i--) {
-                    result.push_back(matrix[down][i]);
-                }
-                down--;
-            }
-            
-            //down to top
-            if(id == 3) {
-                for(int i = down; i>=top; i--) {
-                    result.push_back(matrix[i][left]);
-                }
-                left++;
-            }
-            
-            id = (id+1)%4;
+
+           int top=0;
+           int bottom=m-1;
+
+           int l=0;
+           int r=n-1;
+
+
+           int id=0;
+
+
+    while(l<=r && top<=bottom ){
+
+        if(id==0){
+            for(int i=l;i<=r;i++)  ans.push_back(matrix[top][i]);
+             top++;
         }
-        return result;
+
+         if(id==1){
+
+             for(int i=top;i<=bottom;i++)  ans.push_back(matrix[i][r]);
+
+             r--;
+         }
+          if(id==2){
+             for(int i=r;i>=l;i--) ans.push_back(matrix[bottom][i]);
+
+             bottom--;
+          }
+
+          if(id==3){
+
+             for(int i=bottom;i>=top;i--) ans.push_back(matrix[i][l]);
+             l++;
+          }
+
+
+          id=(id+1)%4;
+    }
+
+        return ans;
+    } 
 
         
-    }
+    
 };
