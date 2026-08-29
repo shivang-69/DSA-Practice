980952c7d4518532093061e784e35810cfe7aae4
shivang-69 <shivang63501@gmail.com>
2026-08-29 17:48:50 +0530

    Time: 39 ms (94.67%) | Memory: 29.9 MB (25.67%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,43 +1,48 @@
 class Solution {
 public:
+   void twosum(vector<int>& nums,vector<vector<int>>&ans,int s, int target){
+    int n=nums.size();
+    int j=s;
+    int k=n-1;
 
-    void twosum(vector<int>& nums,vector<vector<int>>&ans,int s,int target){
-           int j=s;
-           int k=nums.size()-1;
+    while(j<k){
 
-           while(j<k){
-            if(nums[j]+nums[k]>target) k--;
-            else if(nums[j] + nums[k]<target) j++;
+        if(nums[j]+nums[k]>target) k--;
+        else if(nums[j]+nums[k]<target) j++;
 
-            else {
-                ans.push_back({-target,nums[j],nums[k]});
-                while(j<k && nums[j]==nums[j+1]) j++;
-                while(j<k && nums[k]==nums[k-1]) k--;
+        else{
+            ans.push_back({-target,nums[j],nums[k]});
+            while(j<k && nums[j]==nums[j+1]) j++;
+            while(j<k && nums[k]==nums[k-1]) k--;
 
-                j++;
-                k--;
+            j++;
+            k--;
         }
-
-
+   
     }
-}
+   }
+
+
+
+   
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>ans;
-       
+
+
         int n=nums.size();
-        if(n<3) return ans;
+        vector<vector<int>>ans;
 
         sort(nums.begin(),nums.end());
 
-        for(int i=0;i<n-2;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            
-            twosum(nums,ans,i+1,-nums[i]);
-             
+        for(int i=0;i<n;i++){
 
+            if(i>0 && nums[i]==nums[i-1]) continue;
+
+            twosum(nums,ans,i+1,-nums[i]);
         }
 
         return ans;
+
         
+         
     }
 };
