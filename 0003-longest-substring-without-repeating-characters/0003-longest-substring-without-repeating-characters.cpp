cb5217da68aeb7ef4dece7732fab83445b1e5def
shivang-69 <shivang63501@gmail.com>
2026-08-30 16:51:39 +0530

    Time: 62 ms (28.4%), Space: 19.4 MB (27.97%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+
         int n=s.size();
+
+        unordered_map<char,int>mp;
 
         int i=0;
         int j=0;
 
         int maxl=0;
 
-        unordered_map<char,int>mp;
-
         while(j<n){
-
             mp[s[j]]++;
+
             while(mp[s[j]]>1){
                 mp[s[i]]--;
                 i++;
@@ -20,8 +21,10 @@ public:
 
             maxl=max(maxl,j-i+1);
             j++;
-        }
 
+
+
+        }
         return maxl;
         
     }
