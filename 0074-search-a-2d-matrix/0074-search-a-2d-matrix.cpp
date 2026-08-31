8bb46ac8ffae0ab7c39c1effde4a422f06307f65
shivang-69 <shivang63501@gmail.com>
2026-09-01 01:46:05 +0530

    Time: 0 ms (100%), Space: 13.4 MB (6.58%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -7,28 +7,15 @@ public:
 
         int l=0;
         int r=m*n-1;
-      
 
         while(l<=r){
+
             int mid=l+(r-l)/2;
-            int  i=mid/n;
-            int  j=mid%n;
-           
 
-            if(matrix[i][j]==target) return true;
-            else if(matrix[i][j]<target) l=mid+1;
-            else r=mid-1;
-
+            if(matrix[mid/n][mid%n]==target) return true;
+            else if(matrix[mid/n][mid%n]>target) r=mid-1;
+            else l=mid+1;
         }
-
-
-
-
-
         return false;
-
-
-        
     }
 };
-
