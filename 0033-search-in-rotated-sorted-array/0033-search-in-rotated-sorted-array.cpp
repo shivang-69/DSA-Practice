ab26ad889245a1e49892a8a858773e80d6ddcfcf
shivang-69 <shivang63501@gmail.com>
2026-09-14 02:11:47 +0530

    Time: 0 ms (100%), Space: 15.3 MB (37.39%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -8,21 +8,21 @@ public:
         int r=n-1;
 
         while(l<=r){
-
             int mid=l+(r-l)/2;
 
-            if(nums[mid]==target) return mid;
+             if(nums[mid]==target) return mid;
 
-            else if(nums[mid]>=nums[l]){
-                 if(target>=nums[l] && target<nums[mid]) r=mid-1;
-                 else l=mid+1;
+             else if(nums[mid]>=nums[l]){
+                if(target>=nums[l] && target<nums[mid]) r=mid-1;
+                else l=mid+1;
+             }
 
-            }
-            else {
+             else {
                 if(target>nums[mid] && target<=nums[r]) l=mid+1;
                 else r=mid-1;
-            }
+             }
         }
+
         return -1;
         
     }
