8e59d6e8051b193fc25d725320b6413f31d1a0e6
shivang-69 <shivang63501@gmail.com>
2026-09-15 02:23:27 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (24.22%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -3,18 +3,13 @@ public:
     int singleNumber(vector<int>& nums) {
 
         int n=nums.size();
+         
+        int ans=0;
 
-        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++) ans^=nums[i];
+        
 
-        for(int i=0;i<n;i++) mp[nums[i]]++;
-
-        for(auto it: mp){
-            if(it.second==1) return it.first;
-
-        }
-
-        return 0;
-
+        return ans;
         
         
     }
