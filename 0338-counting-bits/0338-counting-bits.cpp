7e599ea7be04d1aa4668088473fe79e52ffbdd73
shivang-69 <shivang63501@gmail.com>
2026-09-16 16:38:41 +0530

    Time: 0 ms (100%), Space: 10.7 MB (57.12%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+
+        vector<int>bits(n+1);
+
+    
+
+        for(int i=1;i<=n;i++)
+            bits[i]=bits[i>>1] + (i&1);
+
+
+
+
+        
+
+        return bits;
+
+        
+        
+    }
+};
